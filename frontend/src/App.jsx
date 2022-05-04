a3c4164f5b0ce463fbcd068fa6b81f8b2588c7c3
FlapShatner <94399441+FlapShatner@users.noreply.github.com>
2022-05-03 23:22:38 -0500

    deploy to heroku

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Notes from './pages/notes'
+import Notes from './pages/Notes'
 import Create from './pages/Create'
 import Header from './components/Header'
 import Edit from './pages/Edit'
