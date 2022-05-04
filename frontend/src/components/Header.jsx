57d6818b3ade19ac2a144ed82a4b8a24340f654e
FlapShatner <94399441+FlapShatner@users.noreply.github.com>
2022-05-03 23:38:40 -0500

    header color changed to indigo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
   }
 
   return (
-    <div className='fixed z-10 w-full max-w-5xl opacity-80 flex flex-row p-1 h-18 bg-gradient-to-r from-sky-500 to-sky-400 justify-between mix-blend-luminosity'>
+    <div className='fixed z-10 w-full max-w-5xl opacity-80 flex flex-row p-1 h-18 bg-gradient-to-r from-indigo-500 to-indigo-400 justify-between mix-blend-luminosity'>
       <Link to={'/'}>
         <div className=' m-2 flex flex-row'>
           <CgNotes className='text-orange-100 h-10 w-10' />
